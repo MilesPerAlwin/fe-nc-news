d1df572f289e1c3cf85c07583a685a0f0fa03b71
MilesPerAlwin <alwincraig@protonmail.com>
2023-07-20 16:15:23 +0100

    will add a loading state message for user to let them know their comment is being posted

--- src/components/PostAComment.jsx
+++ src/components/PostAComment.jsx
@@ -10,6 +10,7 @@ const PostAComment = ({ setCommentsSection }) => {
     const { user, setUser } = useContext(UserContext);
     const [newComment, setNewComment] = useState("");
     const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,13 +18,16 @@ const PostAComment = ({ setCommentsSection }) => {
         if (newComment.length < 1) {
             setIsError(true);
         } else {
-            
+            setIsLoading(true)
             setIsError(false);
             PostComment(newComment, user, article_id)
             .then((postedComment) => {
                 setCommentsSection((currentComments) => {
                     return [postedComment, ...currentComments];
                 })
+            })
+            .then(() => {
+                setIsLoading(false);
             })
             setNewComment("");
         }
@@ -44,6 +48,7 @@ const PostAComment = ({ setCommentsSection }) => {
             </p>
             <p><button 
                 id="comment-button">Post comment</button></p>
+            {isLoading ? <p className="waitPostComment">Thank you, {user}. Please wait while your comment is posted</p> : null}
             {isError ? <p className="errorMessagePostComment">Comment must not be blank. Please try again.</p> : null}
         </form>
     )
